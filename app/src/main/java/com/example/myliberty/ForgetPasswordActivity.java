bc5daf35633c17356ce140fd397337042a60bc8f
Ravneet Singh <ravne31@gmail.com>
2023-06-04 19:09:15 -0400

    Implemented Login, Signup, Forget Password

---
+++ app/src/main/java/com/example/myliberty/ForgetPasswordActivity.java
@@ -0,0 +1,72 @@
+package com.example.myliberty;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class ForgetPasswordActivity extends AppCompatActivity {
+    EditText email;
+    Button resetPassword,login;
+    ProgressBar progressBar;
+    private FirebaseAuth mAuth;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_forget_password);
+        mAuth = FirebaseAuth.getInstance();
+        email=findViewById(R.id.email);
+        resetPassword=findViewById(R.id.resetPassword);
+        login=findViewById(R.id.login);
+        progressBar=findViewById(R.id.progressBar);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+        resetPassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                progressBar.setVisibility(View.VISIBLE);
+                String _email;
+                _email=String.valueOf(email.getText());
+                if(!isValidEmail(_email)){
+                    Toast.makeText(getApplicationContext(),"Please enter valid email",Toast.LENGTH_SHORT).show();
+                    progressBar.setVisibility(View.GONE);
+                    return;
+                }
+               mAuth.sendPasswordResetEmail(_email).addOnCompleteListener(new OnCompleteListener<Void>() {
+                   @Override
+                   public void onComplete(@NonNull Task<Void> task) {
+                       progressBar.setVisibility(View.GONE);
+                       if(task.isSuccessful()){
+                           Toast.makeText(getApplicationContext(),"Reset Password Link sent to your email",Toast.LENGTH_SHORT).show();
+                           email.setText("");
+                       }else{
+                           Toast.makeText(getApplicationContext(),"Failed! Please try again",Toast.LENGTH_SHORT).show();
+                       }
+                   }
+               });
+            }
+        });
+    }
+    public static boolean isValidEmail(CharSequence target) {
+        return (!TextUtils.isEmpty(target) && Patterns.EMAIL_ADDRESS.matcher(target).matches());
+    }
+}
--- app/src/main/java/com/example/myliberty/SignupActivity.java
+++ app/src/main/java/com/example/myliberty/SignupActivity.java
@@ -3,6 +3,7 @@ package com.example.myliberty;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
@@ -11,7 +12,6 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -19,75 +19,90 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
-public class LoginActivity extends AppCompatActivity {
-    EditText email;
-    EditText password;
-    Button login,signup;
-    TextView forgetPassword;
+public class SignupActivity extends AppCompatActivity {
+    private EditText name;
+    private EditText email;
+    private EditText password;
+    private EditText accountNumber;
+    private Button signup,login;
     private FirebaseAuth mAuth;
     private ProgressBar progressBar;
+
+
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_signup);
         mAuth = FirebaseAuth.getInstance();
+        name=findViewById(R.id.name);
         email=findViewById(R.id.email);
         password=findViewById(R.id.password);
-        login=findViewById(R.id.login);
+        accountNumber=findViewById(R.id.accountNumber);
         signup=findViewById(R.id.signup);
-        forgetPassword=findViewById(R.id.forgetPassword);
         progressBar=findViewById(R.id.progressBar);
-        forgetPassword.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(getApplicationContext(),ForgetPasswordActivity.class));
-            }
-        });
-        signup.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(getApplicationContext(),SignupActivity.class));
-            }
-        });
+        login=findViewById(R.id.login);
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+               finish();
+            }
+        });
+
+
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
                 progressBar.setVisibility(View.VISIBLE);
-                String _email,_password;
+                String _email,_password,_accountNumber,_name;
                 _email=String.valueOf(email.getText());
                 _password=String.valueOf(password.getText());
-
-
-
+                _accountNumber=String.valueOf(accountNumber.getText());
+                _name=String.valueOf(name.getText());
+                if(TextUtils.isEmpty(_accountNumber) || _accountNumber.length() != 9||!_accountNumber.matches("[0-9]+")){
+                    Toast.makeText(SignupActivity.this,"Please enter valid account Number",Toast.LENGTH_SHORT).show();
+                    progressBar.setVisibility(View.GONE);
+                    return;
+                }
+                if(TextUtils.isEmpty(_name)||_name.length()<6){
+                    Toast.makeText(SignupActivity.this,"Please enter valid name",Toast.LENGTH_SHORT).show();
+                    progressBar.setVisibility(View.GONE);
+                    return;
+                }
                 if(!isValidEmail(_email)){
-                    Toast.makeText(getApplicationContext(),"Please enter valid email",Toast.LENGTH_SHORT).show();
+                    Toast.makeText(SignupActivity.this,"Please enter valid email",Toast.LENGTH_SHORT).show();
                     progressBar.setVisibility(View.GONE);
                     return;
                 }
                 if(TextUtils.isEmpty(_password)||_password.length()<6){
-                    Toast.makeText(getApplicationContext(),"Please enter valid password ",Toast.LENGTH_SHORT).show();
+                    Toast.makeText(SignupActivity.this,"Password length must be more than 6 ",Toast.LENGTH_SHORT).show();
                     progressBar.setVisibility(View.GONE);
                     return;
                 }
 
-                mAuth.signInWithEmailAndPassword(_email,_password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                mAuth.createUserWithEmailAndPassword(_email,_password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         progressBar.setVisibility(View.GONE);
                         if(task.isSuccessful()){
 
-                            Toast.makeText(getApplicationContext(),"Login Successfully",Toast.LENGTH_SHORT).show();
+                           Toast.makeText(SignupActivity.this,"Account Created Successfully",Toast.LENGTH_SHORT).show();
                             Intent i =new Intent(getApplicationContext(),MainActivity.class);
                             startActivity(i);
                         }
                         else{
-                            Toast.makeText(getApplicationContext(),"Account Creation failed",Toast.LENGTH_SHORT).show();
+                            Toast.makeText(SignupActivity.this,"Account Creation failed",Toast.LENGTH_SHORT).show();
                         }
                     }
                 });
             }
+
         });
+
+
     }
+
     public static boolean isValidEmail(CharSequence target) {
         return (!TextUtils.isEmpty(target) && Patterns.EMAIL_ADDRESS.matcher(target).matches());
     }
