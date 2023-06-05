f2f005dab38fcedefb791030f6c1594c9b0447d3
Ravneet Singh <ravne31@gmail.com>
2023-06-05 08:48:50 -0400

    Created Home,Support,Billing fragment

--- app/src/main/java/com/example/myliberty/MainActivity.java
+++ app/src/main/java/com/example/myliberty/MainActivity.java
@@ -1,14 +1,61 @@
 package com.example.myliberty;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.bottomnavigation.BottomNavigationItemView;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
+
+    BottomNavigationView bottomNavigationView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        bottomNavigationView
+                = findViewById(R.id.bottomNavigationView);
+
+        bottomNavigationView
+                .setOnNavigationItemSelectedListener(this);
+        bottomNavigationView.setSelectedItemId(R.id.home);
+    }
+    HomeFragment homeFragment = new HomeFragment();
+    BillingFragment  billingFragment = new BillingFragment();
+    SupportFragment supportFragment = new SupportFragment();
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.home:
+                getSupportFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.flFragment, homeFragment)
+                        .commit();
+                return true;
+
+            case R.id.support:
+                getSupportFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.flFragment, supportFragment)
+                        .commit();
+                return true;
+
+            case R.id.billing:
+                getSupportFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.flFragment, billingFragment)
+                        .commit();
+                return true;
+        }
+        return false;
+
+    }
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+        super.onPointerCaptureChanged(hasCapture);
     }
 }
---
+++ app/src/main/java/com/example/myliberty/SupportFragment.java
@@ -0,0 +1,31 @@
+package com.example.myliberty;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link SupportFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class SupportFragment extends Fragment {
+
+    // TODO: Rename parameter arguments, choose names that match
+
+    public SupportFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_support, container, false);
+    }
+}
